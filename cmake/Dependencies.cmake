bf51721ae320c0ca3209319483d575d18bec913e
tanxzh <home@ubuntu22.04>
2026-03-28 19:24:42 +0800

    daemon: wire cmake deps and runnable daemon entry

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,1 +1,15 @@
-# Dependency setup will live here as the CMake build grows.
+include(FindPkgConfig)
+
+if (NOT PKG_CONFIG_FOUND)
+    message(FATAL_ERROR "pkg-config is required to resolve native dependencies")
+endif()
+
+pkg_check_modules(LIBWEBSOCKETS REQUIRED IMPORTED_TARGET libwebsockets)
+pkg_check_modules(OPUS REQUIRED IMPORTED_TARGET opus)
+pkg_check_modules(ALSA REQUIRED IMPORTED_TARGET alsa)
+
+set(XIAOZHI_THIRD_PARTY_TARGETS
+    PkgConfig::LIBWEBSOCKETS
+    PkgConfig::OPUS
+    PkgConfig::ALSA
+)
